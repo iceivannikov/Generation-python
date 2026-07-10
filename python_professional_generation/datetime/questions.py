a7719dc039ad247e22cc0fd75afe4d1d7cdadfce
Viktor Ivannikov <iceivannikov@gmail.com>
2026-07-10 14:35:15 +0300

    get_min_max

--- python_professional_generation/datetime/questions.py
+++ python_professional_generation/datetime/questions.py
@@ -1,9 +1,10 @@
 from datetime import date
 
-dates = [date(2010, 9, 28), date(2017, 1, 13), date(2009, 12, 25),
-         date(2010, 2, 27), date(2021, 10, 11), date(2020, 3, 13),
-         date(2000, 7, 7), date(1999, 4, 14), date(1789, 11, 19),
-         date(2013, 8, 21), date(1666, 6, 6), date(1968, 5, 26)]
+def get_min_max(dates):
+    if not dates:
+        return ()
+    return min(dates), max(dates)
 
-for date in dates:
-    print(f"{date.year}-Q{(date.month - 1) // 3 + 1}")
+if __name__ == "__main__":
+    print(get_min_max([date(2021, 10, 5), date(1992, 6, 10), date(2012, 2, 23), date(1995, 10, 12)]))
+    print(get_min_max([]))
