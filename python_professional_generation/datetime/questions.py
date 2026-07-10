be5df718a2e0ef44f74695829df12381bd217004
Viktor Ivannikov <iceivannikov@gmail.com>
2026-07-10 15:16:59 +0300

    get_date_range

--- python_professional_generation/datetime/questions.py
+++ python_professional_generation/datetime/questions.py
@@ -1,10 +1,16 @@
-from datetime import date
+from datetime import date, timedelta
 
-def get_min_max(dates):
-    if not dates:
-        return ()
-    return min(dates), max(dates)
+def get_date_range(date_1, date_2):
+    result = []
+    if date_1 > date_2:
+        return result
+    n = (date_2 - date_1).days
+    for i in range(n + 1):
+        new_date = date_1 + timedelta(days=i)
+        result.append(new_date)
+    return result
+
 
 if __name__ == "__main__":
-    print(get_min_max([date(2021, 10, 5), date(1992, 6, 10), date(2012, 2, 23), date(1995, 10, 12)]))
-    print(get_min_max([]))
+    print(*get_date_range(date(2021, 10, 1), date(2021, 10, 5)), sep="\n")
+    print(*get_date_range(date(2019, 6, 5), date(2019, 6, 5)))
