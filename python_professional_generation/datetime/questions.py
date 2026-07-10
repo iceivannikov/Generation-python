5155f8933220386820a4f47f7b6ddc0452d57883
Viktor Ivannikov <iceivannikov@gmail.com>
2026-07-10 15:58:23 +0300

    saturdays_between_two_dates

--- python_professional_generation/datetime/questions.py
+++ python_professional_generation/datetime/questions.py
@@ -1,16 +1,19 @@
 from datetime import date, timedelta
 
-def get_date_range(date_1, date_2):
-    result = []
-    if date_1 > date_2:
-        return result
-    n = (date_2 - date_1).days
-    for i in range(n + 1):
-        new_date = date_1 + timedelta(days=i)
-        result.append(new_date)
+
+def saturdays_between_two_dates(date_1, date_2):
+    start = min(date_1, date_2)
+    end = max(date_1, date_2)
+    result = 0
+    n = (end - start).days
+    for _ in range(n + 1):
+        if start.weekday() == 5:
+            result += 1
+        start += timedelta(days=1)
     return result
 
 
 if __name__ == "__main__":
-    print(*get_date_range(date(2021, 10, 1), date(2021, 10, 5)), sep="\n")
-    print(*get_date_range(date(2019, 6, 5), date(2019, 6, 5)))
+    print(saturdays_between_two_dates(date(2021, 11, 1), date(2021, 11, 22)))
+    print(saturdays_between_two_dates(date(2020, 7, 26), date(2020, 7, 2)))
+    print(saturdays_between_two_dates(date(2018, 7, 13), date(2018, 7, 13)))
