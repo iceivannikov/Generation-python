f8319b65618e3782a5d4b5ca3b04bba17ba264f1
Viktor Ivannikov <iceivannikov@gmail.com>
2026-07-14 16:10:24 +0300

    is_correct

--- python_professional_generation/datetime/questions.py
+++ python_professional_generation/datetime/questions.py
@@ -1,11 +1,13 @@
 from datetime import date
 
-def print_good_dates(dates):
-    for dt in sorted(dates):
-        if dt.year == 1992 and dt.day + dt.month == 29:
-            print(dt.strftime("%B %d, %Y"))
+def is_correct(day, month, year):
+    try:
+        date(year, month, day)
+        return True
+    except ValueError:
+        return False
 
 
 if __name__ == "__main__":
-    print_good_dates([date(1992, 10, 19), date(1991, 12, 6), date(1992, 9, 20)])
-    print_good_dates([date(1993, 9, 15), date(2021, 11, 2), date(2000, 7, 7)])
+    print(is_correct(31, 12, 2021))
+    print(is_correct(31, 13, 2021))
