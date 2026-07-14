1f6661f67f5f9a81ca8c41af9baf01991466043a
Viktor Ivannikov <iceivannikov@gmail.com>
2026-07-14 16:03:12 +0300

    print_good_dates

--- python_professional_generation/datetime/questions.py
+++ python_professional_generation/datetime/questions.py
@@ -1,8 +1,11 @@
 from datetime import date
 
+def print_good_dates(dates):
+    for dt in sorted(dates):
+        if dt.year == 1992 and dt.day + dt.month == 29:
+            print(dt.strftime("%B %d, %Y"))
+
 
 if __name__ == "__main__":
-    n = int(input())
-    lst = [date.fromisoformat(input()) for _ in range(n)]
-    for dt in sorted(lst):
-        print(dt.strftime("%d/%m/%Y"))
+    print_good_dates([date(1992, 10, 19), date(1991, 12, 6), date(1992, 9, 20)])
+    print_good_dates([date(1993, 9, 15), date(2021, 11, 2), date(2000, 7, 7)])
