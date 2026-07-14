48fee694cc22493bd76d490f88c84b60cd2630d2
Viktor Ivannikov <iceivannikov@gmail.com>
2026-07-14 15:42:38 +0300

    sorted_dates

--- python_professional_generation/datetime/questions.py
+++ python_professional_generation/datetime/questions.py
@@ -1,12 +1,8 @@
 from datetime import date
 
-def two_dates(date1, date2):
-    first = date.fromisoformat(date1)
-    second = date.fromisoformat(date2)
-    return min(first, second).strftime("%d-%m (%Y)")
-
 
 if __name__ == "__main__":
-    print(two_dates("2021-05-12", "2021-05-04"))
-    print(two_dates("1999-07-14", "1999-07-14"))
-    print(two_dates("2026-12-18", "2026-10-01"))
+    n = int(input())
+    lst = [date.fromisoformat(input()) for _ in range(n)]
+    for dt in sorted(lst):
+        print(dt.strftime("%d/%m/%Y"))
